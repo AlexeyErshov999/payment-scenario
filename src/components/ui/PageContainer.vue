c55dacd02807dafc0b8afbf24c0991716449953a
Alexey <eaa04@bk.ru>
2026-02-20 19:12:45 +0300

    add: PageContainer for centering page content

---
+++ src/components/ui/PageContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="center" :class="{ 'full-viewport': fullViewport }">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  fullViewport?: boolean
+}>()
+</script>
+
+<style lang="scss" scoped>
+.center {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &.full-viewport {
+    block-size: 100vh;
+  }
+}
+</style>
