e1883d8f4e2fb97bb79d2d5c96a137ba21a0de41
Alexey Ershov <eaa04@bk.ru>
2026-02-24 12:41:55 +0300

    add: github pager deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
+  base: '/payment-scenario/', // Важно: слэш в начале и конце
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
